df4bd7882addafb4d14ef36338065296f0c216a3
Olga Gorban <oatyanina17@gmail.com>
2026-08-11 13:26:10 +0300

    Check meeting date in notification

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,6 +1,5 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -18,11 +17,9 @@ public class CardDeliveryTest {
 
         open("http://localhost:9999");
 
-        // Город
         $("[data-test-id='city'] input")
                 .setValue("Москва");
 
-        // Дата — через 3 дня от текущей
         String date = LocalDate.now()
                 .plusDays(3)
                 .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
@@ -31,27 +28,23 @@ public class CardDeliveryTest {
                 .doubleClick()
                 .sendKeys(date);
 
-        // Фамилия и имя
         $("[data-test-id='name'] input")
                 .setValue("Иванов Иван");
 
-        // Телефон
         $("[data-test-id='phone'] input")
                 .setValue("+79999999999");
 
-        // Согласие
         $("[data-test-id='agreement']")
                 .click();
 
-        // Отправка формы
         $$("button")
                 .findBy(text("Забронировать"))
                 .shouldBe(visible)
                 .click();
 
-        // Проверяем успешное бронирование
         $("[data-test-id='notification']")
                 .shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(text("Успешно!"));
+                .shouldHave(text("Успешно!"))
+                .shouldHave(text(date));
     }
 }
